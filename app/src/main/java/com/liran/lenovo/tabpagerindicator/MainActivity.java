197051c52349678575f2062b2cf91e1ea3069c84
liran <qq997843911@gmail.com>
2016-02-06 20:15:09 +0800

    大功告成

--- app/src/main/java/com/liran/lenovo/tabpagerindicator/MainActivity.java
+++ app/src/main/java/com/liran/lenovo/tabpagerindicator/MainActivity.java
@@ -1,14 +1,61 @@
 package com.liran.lenovo.tabpagerindicator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private ViewPager viewPager;
+    private ViewPagerIndicator viewPagerIndicator;
+    private List<String> mDatas = Arrays.asList("短信", "收藏", "推荐");
+    private FragmentPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initDatas();
+
+        viewPagerIndicator.setTabItemTitles(mDatas);
+        viewPager.setAdapter(adapter);
+        viewPagerIndicator.setViewPager(viewPager, 0);
+    }
+
+    private void initView() {
+        viewPager = (ViewPager) findViewById(R.id.id_vp);
+        viewPagerIndicator = (ViewPagerIndicator) findViewById(R.id.id_indicator);
+    }
+
+    private void initDatas() {
+        for (String data : mDatas) {
+            VpSimpleFragment fragment = VpSimpleFragment.newInstance(data);
+            fragmentList.add(fragment);
+        }
+
+        adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragmentList.size();
+            }
+        };
 
 
     }
--- app/src/main/java/com/liran/lenovo/tabpagerindicator/ViewPagerIndicator.java
+++ app/src/main/java/com/liran/lenovo/tabpagerindicator/ViewPagerIndicator.java
@@ -4,20 +4,26 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.drawable.Drawable;
 import android.support.v4.view.ViewPager;
-import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.List;
 
 /**
  * TODO: document your custom view class.
  */
-public class ViewPagerIndicator extends View {
+public class ViewPagerIndicator extends LinearLayout {
 
     /**
      * 绘制三角形的画笔
@@ -44,7 +50,7 @@ public class ViewPagerIndicator extends View {
     /**
      * 三角形的最大宽度
      */
-//    private final int DIMENSION_TRIANGEL_WIDTH = (int) (getScreenWidth() / 3 * RADIO_TRIANGEL);
+    private final int DIMENSION_TRIANGEL_WIDTH = (int) (getScreenWidth() / 3 * RADIO_TRIANGEL);
 
     /**
      * 初始时三角形指示器的偏移量
@@ -57,12 +63,12 @@ public class ViewPagerIndicator extends View {
     private int mTranslationX;
 
     /**
-     * 默认的tab数量
+     * 默认可见的tab数量
      */
     private static final int COUNT_DEFAULT_TAB = 4;
 
     /**
-     * tab数量
+     * 可见的tab数量
      */
     private int mTabVisibleCount = COUNT_DEFAULT_TAB;
 
@@ -84,16 +90,22 @@ public class ViewPagerIndicator extends View {
      */
     private static final int COLOR_TEXT_HIGHLIGHTCOLOR = 0xFFFFFFFF;
 
+    /**
+     * 对外的viewpager的回调接口
+     */
+    public interface PageChangeListener {
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels);
 
+        public void onPageSelected(int position);
 
-    private String mExampleString; // TODO: use a default from R.string...
-    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-    private Drawable mExampleDrawable;
+        public void onPageScrollStateChanged(int state);
+    }
 
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+    /**
+     * 对外的ViewPager的回调接口
+     */
+    private PageChangeListener onPageChangeListener;
+
 
     public ViewPagerIndicator(Context context) {
         super(context);
@@ -111,45 +123,166 @@ public class ViewPagerIndicator extends View {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
+
         // Load attributes
         final TypedArray a = getContext().obtainStyledAttributes(
                 attrs, R.styleable.ViewPagerIndicator, defStyle, 0);
-
-        mExampleString = a.getString(
-                R.styleable.ViewPagerIndicator_exampleString);
-        mExampleColor = a.getColor(
-                R.styleable.ViewPagerIndicator_exampleColor,
-                mExampleColor);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.ViewPagerIndicator_exampleDimension,
-                mExampleDimension);
-
-        if (a.hasValue(R.styleable.ViewPagerIndicator_exampleDrawable)) {
-            mExampleDrawable = a.getDrawable(
-                    R.styleable.ViewPagerIndicator_exampleDrawable);
-            mExampleDrawable.setCallback(this);
+        mTabVisibleCount = a.getInt(R.styleable.ViewPagerIndicator_item_count, COUNT_DEFAULT_TAB);
+        if (mTabVisibleCount <= 0) {
+            mTabVisibleCount = COUNT_DEFAULT_TAB;
         }
-
         a.recycle();
 
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
+        //初始化画笔
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.parseColor("#ffffffff"));
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setPathEffect(new CornerPathEffect(3));
     }
 
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
+    /**
+     * 设置可见的tab的数量
+     *
+     * @param count
+     */
+    public void setVisibleTabCount(int count) {
+        this.mTabVisibleCount = count;
+    }
 
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
+    /**
+     * 设置tab的标题内容 可选，可以自己在布局文件中写死
+     *
+     * @param datas
+     */
+    public void setTabItemTitles(List<String> datas)
+    {
+        // 如果传入的list有值，则移除布局文件中设置的view
+        if (datas != null && datas.size() > 0)
+        {
+            this.removeAllViews();
+            this.mTabTitles = datas;
+
+            for (String title : mTabTitles)
+            {
+                // 添加view
+                addView(generateTextView(title));
+            }
+            // 设置item的click事件
+            setItemClickEvent();
+        }
+
+    }
+
+    private View generateTextView(String title) {
+
+        TextView tv=new TextView(getContext());
+        LinearLayout.LayoutParams lp=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lp.width=getScreenWidth()/mTabVisibleCount;
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextColor(COLOR_TEXT_NORMAL);
+        tv.setText(title);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+        tv.setLayoutParams(lp);
+        return tv;
+    }
+
+
+    /**
+     * 对外的ViewPager的回调接口的设置
+     *
+     * @param pageChangeListener
+     */
+    public void setOnPageChangeListener(PageChangeListener pageChangeListener) {
+        this.onPageChangeListener = pageChangeListener;
+    }
+
+    /**
+     * 设置布局view中的一些必要属性，如果设置了setTabTitles，布局中的view则无效
+     */
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        int count = getChildCount();
+
+        if (count == 0)
+            return;
+
+        for (int i = 0; i < count; i++) {
+            View view = getChildAt(i);
+            LinearLayout.LayoutParams lp = (LayoutParams) view.getLayoutParams();
+            lp.weight = 0;
+            lp.width = getScreenWidth() / mTabVisibleCount;
+            view.setLayoutParams(lp);
+        }
+
+        //设置点击事件
+        setItemClickEvent();
+    }
+
+    /**
+     * 设置点击事件
+     */
+    private void setItemClickEvent() {
+
+        int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            final int j = i;
+            View view = getChildAt(i);
+            view.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mViewPager.setCurrentItem(j);
+                }
+            });
+        }
+
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        /**
+         * 初始化三角形的宽度
+         */
+        mTriangleWidth = (int) (w / mTabVisibleCount * RADIO_TRIANGEL);
+
+        //保证宽度不超过最大宽度
+        mTriangleWidth = Math.min(DIMENSION_TRIANGEL_WIDTH, mTriangleWidth);
+
+        //初始化三角形
+        initTriangle();
+
+        mInitTranslationX = w / mTabVisibleCount / 2 - mTriangleWidth / 2;
+
+    }
+
+    /**
+     * 初始化三角形
+     */
+    private void initTriangle() {
+        mPath = new Path();
+        mTriangleHeight = (int) (mTriangleWidth / 2 / Math.sqrt(2));
+
+        mPath.moveTo(0, 0);
+        mPath.lineTo(mTriangleWidth, 0);
+        mPath.lineTo(mTriangleWidth / 2, -mTriangleHeight);
+        mPath.close();
+    }
+
+
+    //绘制指示器
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+
+        canvas.save();
+        canvas.translate(mInitTranslationX + mTranslationX, getHeight() + 1);
+        canvas.drawPath(mPath, mPaint);
+        canvas.restore();
+        super.dispatchDraw(canvas);
+
     }
 
     @Override
@@ -167,7 +300,7 @@ public class ViewPagerIndicator extends View {
         int contentHeight = getHeight() - paddingTop - paddingBottom;
 
         // Draw the text.
-        canvas.drawText(mExampleString,
+   /*     canvas.drawText(mExampleString,
                 paddingLeft + (contentWidth - mTextWidth) / 2,
                 paddingTop + (contentHeight + mTextHeight) / 2,
                 mTextPaint);
@@ -177,8 +310,114 @@ public class ViewPagerIndicator extends View {
             mExampleDrawable.setBounds(paddingLeft, paddingTop,
                     paddingLeft + contentWidth, paddingTop + contentHeight);
             mExampleDrawable.draw(canvas);
+        }*/
+
+
+    }
+
+
+    /**
+     * 获取屏幕宽
+     *
+     * @return 宽
+     */
+    public int getScreenWidth() {
+
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
+    public void setViewPager(ViewPager viewPager, int pos) {
+        this.mViewPager = viewPager;
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                //滚动
+                scroll(position, positionOffset);
+
+                if (onPageChangeListener != null) {
+                    onPageChangeListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                }
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //设置字体颜色高亮
+                resetTextviewColor();
+                highLightTextView(position);
+                if (onPageChangeListener != null) {
+                    onPageChangeListener.onPageSelected(position);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (onPageChangeListener != null) {
+                    onPageChangeListener.onPageScrollStateChanged(state);
+                }
+            }
+        });
+
+        //设置当前页
+        mViewPager.setCurrentItem(pos);
+        //高亮
+        highLightTextView(pos);
+    }
+
+    /**
+     * 指示器跟随手指，以及容器滚动
+     *
+     * @param position
+     * @param positionOffset
+     */
+    private void scroll(int position, float positionOffset) {
+
+        //不断改变偏移量
+        mTranslationX = (int) (getWidth() / mTabVisibleCount * (position + positionOffset));
+
+        int tabWidth = getScreenWidth() / mTabVisibleCount;
+
+        //容器滚动
+        if (getChildCount() > mTabVisibleCount && positionOffset > 0 && position >= (mTabVisibleCount - 2)) {
+            if (mTabVisibleCount != 1) {
+                this.scroll((position - (mTabVisibleCount - 2)) * tabWidth + (int) (tabWidth * positionOffset), 0);
+            } else {
+                this.scroll(position * tabWidth + (int) (tabWidth + positionOffset), 0);
+            }
+
+        }
+        invalidate();
+
+    }
+
+    /**
+     * 高亮文本
+     *
+     * @param position
+     */
+    private void highLightTextView(int position) {
+        View view = getChildAt(position);
+        if (view instanceof TextView) {
+            ((TextView) view).setTextColor(COLOR_TEXT_HIGHLIGHTCOLOR);
         }
     }
 
+    /**
+     * 重置文本
+     */
+    private void resetTextviewColor() {
+
+        for (int i = 0; i < getChildCount(); i++) {
+            View view = getChildAt(i);
+            if (view instanceof TextView) {
+                ((TextView) view).setTextColor(COLOR_TEXT_NORMAL);
+            }
+        }
+
+    }
 
 }
